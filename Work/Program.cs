8405863ca379dee0c0664fedeff60b5e0637242e
XFX <sah0329@mail.ru>
2022-08-07 21:15:36 +0500

    the end?

--- Work/Program.cs
+++ Work/Program.cs
@@ -1,8 +1,15 @@
-﻿string[] Fork = { "World", "Sam", "BibaAndBoba", "777" };
+﻿string[] Fork = { "World", "Sam", "BibaAndBoba", "777","{}","Bs" };
+Console.WriteLine("[" + string.Join(",", Fork) + "]");
+int m =0;
 for(int i=0, n=0;i<Fork.Length;i++){
     n = Fork[i].Length;
-    Console.WriteLine(n);
+    if(n<4){
+        Fork[m]=Fork[i];
+        m++;
+    }
 }
+for(int i=0; i<m;i++) 
+    Console.Write(" "+Fork[i]);
 // string For = "World";
 // int n;
 // n = For.Length;
