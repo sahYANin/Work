33d3310b19bc8f232c7bc674ba86c34cf69d258a
XFX <sah0329@mail.ru>
2022-08-07 20:58:49 +0500

    tests

--- Work/Program.cs
+++ Work/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] Fork = { "World", "Sam", "BibaAndBoba", "777" };
+// for(int i=0;i<Fork.Length;i++){
+
+// }
+string For = "World";
+int n;
+n = For.Length;
+
+Console.WriteLine(n);
