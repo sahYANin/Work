68c1a77192c5974df2f7ed197c84fb5a374b827d
XFX <sah0329@mail.ru>
2022-08-07 21:01:19 +0500

    test 2

--- Work/Program.cs
+++ Work/Program.cs
@@ -1,9 +1,9 @@
 ﻿string[] Fork = { "World", "Sam", "BibaAndBoba", "777" };
-// for(int i=0;i<Fork.Length;i++){
+for(int i=0, n=0;i<Fork.Length;i++){
+    n = Fork[i].Length;
+    Console.WriteLine(n);
+}
+// string For = "World";
+// int n;
+// n = For.Length;
 
-// }
-string For = "World";
-int n;
-n = For.Length;
-
-Console.WriteLine(n);
